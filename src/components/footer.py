69b57a9b43d9209f480d17600365396cd1f60edc
Aman kumar <aman.kumar192546@gmail.com>
2026-06-24 22:40:39 +0530

    added ui components for home screen

---
+++ src/components/footer.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def footer_home():
+    # logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
+
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-aligne:center;">
+            <p style="font-weight:bold; color:white;"> Created with Love Aman</p>
+        </div>
+    
+    """, unsafe_allow_html=True)
