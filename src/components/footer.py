50a52f9c55f52980e717251f9d6b521a4660fcf6
Aman kumar <aman.kumar192546@gmail.com>
2026-07-03 20:28:58 +0530

    Integrated manage subjects dialog on teacher dashboard

--- src/components/footer.py
+++ src/components/footer.py
@@ -16,7 +16,7 @@ def footer_dashboard():
 
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-aligne:center;">
-            <p style="font-weight:bold; color:black;"> Created with Love Aman</p>
+            <p style="font-weight:bold; color:black;"> Created with ❤ Aman</p>
         </div>
     
     """, unsafe_allow_html=True)
