bdf4519708e4e310f3631d04939e6a91f784240f
Aman kumar <aman.kumar192546@gmail.com>
2026-07-14 21:14:33 +0530

    added functionality to take photos for attendance

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -6,6 +6,7 @@ from src.database.db import check_teacher_exists, create_teacher, teacher_login,
 from src.components.dialog_create_subject import create_subject_dialog
 from src.components.subject_card import subject_card
 from src.components.dialog_share_subject import share_subject_dialog
+from src.components.dialog_add_photo import add_photos_dialog
 
 
 def teacher_screen():
@@ -72,7 +73,32 @@ def teacher_dashboard():
 
 
 def teacher_tab_take_attendance():
+    teacher_id = st.session_state.teacher_data['teacher_id']
     st.header("Take AI Attendance")
+
+    if 'attendance_images' not in st.session_state:
+        st.session_state.attendance_images = []
+
+    subjects = get_teacher_subject(teacher_id)
+
+    if not subjects:
+        st.warning("You haven't created any subjects yet! Please create one to begin!")
+        return 
+    
+    subject_options = {f"{s['name']} - {s['subject_code']}": s['subject_id'] for s in subjects}
+
+    col1, col2 = st.columns([3,1])
+
+    with col1:
+        selected_subject_label = st.selectbox('Select Subject', options=list(subject_options.keys()))
+
+    with col2:
+        if st.button('Add Photos', type='primary', icon=':material/photo_prints:', width='stretch'):
+            add_photos_dialog()
+
+    selected_subject_id = subject_options[selected_subject_label]
+
+    st.divider()
 
 
 def teacher_tab_manage_subjects():
@@ -95,18 +121,18 @@ def teacher_tab_manage_subjects():
                 ("🕗", "Classes", sub['total_classes'])
             ]
 
-        def share_btn():
-            if st.button(f"Share Code: {sub['name']}", key=f"share_{sub['subject_code']}", icon=":material/share:"):
-                share_subject_dialog(sub['name'], sub['subject_code'])
-            st.space()
+            def share_btn():
+                if st.button(f"Share Code: {sub['name']}", key=f"share_{sub['subject_code']}", icon=":material/share:"):
+                    share_subject_dialog(sub['name'], sub['subject_code'])
+                st.space()
 
-        subject_card(
-            name = sub['name'],
-            code = sub['subject_code'],
-            section = sub['section'],
-            stats = stats,
-            footer_callback = share_btn
-        )
+            subject_card(
+                name = sub['name'],
+                code = sub['subject_code'],
+                section = sub['section'],
+                stats = stats,
+                footer_callback = share_btn
+            )
     else:
         st.warning("NO SUBJECTS FOUND, CREATE ONE ABOVE")
 
